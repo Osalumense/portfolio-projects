f3ca11fcaa98ad590e41dc12f2455bc1716272eb
STEPHEN <harkugbeosaz@gmail.com>
2025-05-26 15:33:56 +0200

    Feat: Updated lists image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Stephen Akugbe portfolio projects',
+  description: 'List of projects I have created over the past 5 years'
 }
 
 export default function RootLayout({
